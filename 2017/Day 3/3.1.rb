cf3b27ebc3affe062a2b299adce3670a46d8460c
lexvenim <lexvenim@gmail.com>
2017-12-20 13:59:59 +0300

    Day 3 less code

--- 2017/Day 3/3.1.rb
+++ 2017/Day 3/3.1.rb
@@ -1,14 +1,8 @@
 num = 361527
 
-sqrt = Math.sqrt(num).ceil
-sqrt +=1 if sqrt % 2 != 1
+circle = Math.sqrt(num).ceil / 2
+sqrt = (circle * 2 + 1)**2
+loop { sqrt -= (circle*2); break if sqrt < num }
+p circle + (num - (sqrt + circle)).abs
 
-
-diff = (sqrt**2 - num) % (sqrt - 1)
-diff = ((sqrt - 1) - diff) if diff > ((sqrt - 1) / 2)
-
-res = (sqrt / 2).floor * 2 - diff
-
-p res
-
-# res = 326
+# steps = 326
--- 2017/Day 3/3.2.rb
+++ 2017/Day 3/3.2.rb
@@ -1,59 +1,22 @@
 num = 361527
 
-arr = {"0,0": 1, "1,0": 1}
+arr = {"0,0": 1}
 
-sqrt = 3
+answer = 1.step do |n|
+	next if n == 1
+	circle = Math.sqrt(n).ceil / 2
+	side_border = (circle*2 - 1)**2
 
-direction = 0
+	side = 1.step { |i| side_border += circle*2; break i if side_border >= n }
+	x,y = circle, circle - (side_border - n)
+	x = -x if side > 2
+	y = -y if side.between?(2,3)
+	x,y = y,x if side.even?
 
-x = 1
-y = 0
+	res = arr[(x.to_s + ',' + y.to_s).to_sym] = [x.pred, x, x.next].product([y.pred, y, y.next]).inject(0) { |sum, (ax, ay)| key = ax.to_s + ',' + ay.to_s; sum += arr[key.to_sym] if arr[key.to_sym]; sum }
 
-answer = 0
-
-while (true)
-	new_spiral = false
-
-	if (x == (sqrt/2).ceil) && (y == -((sqrt/2).ceil))
-		sqrt += 2
-		direction = 0
-		x += 1
-		new_spiral = true
-	end
-
-	if !new_spiral
-		direction = (direction + 1) % 4 if (x.abs == (sqrt/2).ceil) && (y.abs == (sqrt/2).ceil)
-
-		case direction
-		when 0
-			y += 1
-		when 1
-			x -= 1
-		when 2
-			y -= 1
-		else
-			x += 1
-		end
-	end
-
-	sum = 0
-	(-1..1).each do |i|
-		(-1..1).each do |j|
-			next if i == 0 && j == 0
-			key = (x + i).to_s + ',' + (y + j).to_s
-			sum += arr[key.to_sym] if arr[key.to_sym]
-		end
-	end
-
-	arr[(x.to_s + ',' + y.to_s).to_sym] = sum
-
-	if (sum > num)
-		answer = sum
-		break
-	end
-
+	break res if res > num
 end
 
 p answer
-
 # answer = 363010
