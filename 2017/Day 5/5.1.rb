f18240d7af8aa2e2e7d60f72b29dd4fc094e0f2c
lexvenim <lexvenim@gmail.com>
2017-12-20 14:23:53 +0300

    Day 5 less code

--- 2017/Day 5/5.1.rb
+++ 2017/Day 5/5.1.rb
@@ -1,18 +1,12 @@
-maze = []
-
-File.open("input.txt").each_line {|l| maze.push l.to_i}
+maze = File.open("input.txt", "r").readlines.map{|n| n.to_i}
 
 i = 0
-step = 0
-
-while i < maze.length do
-	step += 1
+step = 1.step do |s|
 	now = maze[i]
-
 	maze[i] += 1
 	next if now == 0
-
 	i += now
+	break s if i >= maze.length
 end
 
 p step
--- 2017/Day 5/5.2.rb
+++ 2017/Day 5/5.2.rb
@@ -1,20 +1,12 @@
-maze = []
-
-File.open("input.txt").each_line {|l| maze.push l.to_i}
+maze = File.open("input.txt", "r").readlines.map{|n| n.to_i}
 
 i = 0
-step = 0
-
-while i < maze.length do
-	step += 1
-
+step = 1.step do |s|
 	now = maze[i]
 	maze[i] >= 3 ? maze[i] -= 1 : maze[i] += 1
-
-	p step.to_s + " ... " + i.to_s + ": " + now.to_s
 	next if now == 0
-
 	i += now
+	break s if i >= maze.length
 end
 
 p step
